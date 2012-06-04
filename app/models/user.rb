7e04d2b44880bf28af6e351d283d5cda843e4f74
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-06-04 16:47:53 -0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-  
+    
   # Both of the below do the same
   # before_save { |user| user.email = email.downcase }
   before_save { self.email.downcase! }
+  before_save :create_remember_token
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -24,4 +25,9 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
