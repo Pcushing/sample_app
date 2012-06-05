999b497543c50b67032fa06ea08aa5a7c80612ac
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-06-04 22:21:46 -0700

    Refactored sign in form with form_at

---
+++ spec/helpers/user_helper_spec.rb
@@ -0,0 +1,13 @@
+# require 'spec_helper'
+# 
+# describe UserHelper do
+# 
+#   describe valid_signin(user)
+#     fill_in "Name",         with: "Example User"
+#     fill_in "Email",        with: "user@example.com"
+#     fill_in "Password",     with: "foobar"
+#     fill_in "Confirmation", with: "foobar"
+#     click_button "Create my account"
+#   end
+# 
+# end
