36a33112595c942935bef54296c3b694859a58ee
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-06-04 22:21:16 -0700

    Refactored sign in form with form_at

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,22 +13,19 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
       
-      it { should have_selector('h1',                     text: 'Sign in')}
-      it { should have_selector('div.alert.alert-error',  text: 'Invalid')}
+      it { should have_selector('h1',   text: 'Sign in')}
+      it { should have_error_message('Invalid')}
     
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
+        # it { should_not have_selector('div.alert.alert-error') } Removed in favor of the line below, no params necessary
+        it { should_not have_error_message }
       end
     end
     
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { valid_signin(user) }
       
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,4 @@
 include ApplicationHelper
+include SessionsHelper
+# include UserHelper
+
